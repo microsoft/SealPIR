c7916baa05b43a5c96b8de094232a64f168515a2
Sebastian Angel <sga001@cis.upenn.edu>
2021-08-26 14:15:48 -0700

    fixing test bug and patching readme. Also deleting files

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrew/Documents/Research/AC-PIR/SealPIR
-# Build directory: /home/andrew/Documents/Research/AC-PIR/SealPIR
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
